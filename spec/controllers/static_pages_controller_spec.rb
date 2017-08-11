963e296f18156e988df636e97404584b81c930ca
yutainoue <inoue_yuta@nexway.co.jp>
2017-08-11 17:04:17 +0900

    各ページにタイトルを加えた

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe StaticPagesController, :type => :controller do
+RSpec.describe StaticPagesController, type: :controller do
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
-  describe "GET home" do
-    it "returns http success" do
-      get :home
+  describe 'home' do
+    before { get :home }
+
+    it '表示に成功する' do
+      assert_select 'title', "Home | #{base_title}"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET help" do
-    it "returns http success" do
-      get :help
+  describe 'help' do
+    before { get :help }
+
+    it '表示に成功する' do
       expect(response).to have_http_status(:success)
+      assert_select 'title', "Help | #{base_title}"
     end
   end
 
+  describe 'about' do
+    before { get :about }
+
+    it '表示に成功する' do
+      expect(response).to have_http_status(:success)
+      assert_select 'title', "About | #{base_title}"
+    end
+  end
 end
