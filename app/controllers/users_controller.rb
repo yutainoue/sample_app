205326d442f0b03fe12c149ebab9ed6654025c6c
yutainoue <inoue_yuta@nexway.co.jp>
2017-08-21 22:37:34 +0900

    タイポ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = `会員登録おめでとうー`
+      flash[:success] = '会員登録おめでとうー'
       redirect_to user_url(@user)
     else
       render 'new'
