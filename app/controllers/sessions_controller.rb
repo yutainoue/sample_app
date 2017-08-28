746415d78fe94b4c6d753cec033de85b35bc183d
yutainoue <inoue_yuta@nexway.co.jp>
2017-08-28 22:46:47 +0900

    ログイン失敗時にエラー表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
+
+  def destroy; end
 end
