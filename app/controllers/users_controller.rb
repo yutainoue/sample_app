6747c86fd822b5a2e47daa5f0cfbae45adc79589
yutainoue <inoue_yuta@nexway.co.jp>
2017-08-28 23:16:28 +0900

    サインインしたらログインする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = '会員登録おめでとうー'
       redirect_to user_url(@user)
     else
