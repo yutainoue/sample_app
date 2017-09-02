653caba8ead9fe8b9b66ea728e90b5a47932e792
yutainoue <inoue_yuta@nexway.co.jp>
2017-09-02 16:03:23 +0900

    ユーザ一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[edit update]
+  before_action :logged_in_user, only: %i[edit update index]
   before_action :correct_user,   only: %i[edit update]
+
+  def index
+    @users = User.all
+  end
 
   def new
     @user = User.new
