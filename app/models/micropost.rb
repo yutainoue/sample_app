8745953b3f4c676915909d53104d31816083db80
yutainoue <inoue_yuta@nexway.co.jp>
2017-09-21 22:45:58 +0900

    5MB以上は許容しないようにした

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,10 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+
+  def picture_size
+    return unless picture.size > 5.megabytes
+    errors.add(:picture, 'should be less than 5MB')
+  end
 end
