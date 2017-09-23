eeaf16e3a63a7dcd5609e91c21c28cf19d010583
yutainoue <inoue_yuta@nexway.co.jp>
2017-09-23 12:44:55 +0900

    フォロー関連メソッドの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_many :microposts
+  has_many :active_relationships, class_name:  'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -10,6 +16,7 @@ class User < ApplicationRecord
                        format: { with: VALID_EMAIL_REGEX },
                        uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+
   has_secure_password
 
   def self.digest(string)
@@ -62,6 +69,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
